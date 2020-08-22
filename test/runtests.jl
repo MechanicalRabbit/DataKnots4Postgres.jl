56cb59046a51ef8e0b3760288cc3c7d8e3768173
Clark C. Evans <cce@clarkevans.com>
2020-08-22 15:18:57 -0500

    Remove PostgreSQL on Ubuntu environment variable.

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,11 +4,5 @@ using DataKnots
 using DataKnots4Postgres
 using NarrativeTest
 
-if !haskey(ENV, "PGHOST") && Sys.islinux()
-  if endswith(strip(read(`/usr/bin/lsb_release --id`, String)), "Ubuntu")
-     ENV["PGHOST"] = "/var/run/postgresql"
-  end
-end
-
 args = !isempty(ARGS) ? ARGS : [relpath(joinpath(dirname(abspath(PROGRAM_FILE)), "../doc/src"))]
 exit(!runtests(args))
